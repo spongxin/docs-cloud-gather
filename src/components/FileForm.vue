6346ef9bd7f8bea92c98a3149768997c9692a5e6
spongxin <2624490979@qq.com>
2022-04-14 22:03:38 +0800

    init

---
+++ src/components/FileForm.vue
@@ -0,0 +1,23 @@
+<template>
+  <v-container fluid>
+    <v-alert
+      border="top"
+      colored-border
+      type="info"
+      elevation="2"
+    >
+      Vestibulum ullamcorper mauris at ligula. Nam pretium turpis et arcu. Ut varius tincidunt libero. Curabitur ligula sapien, tincidunt non, euismod vitae, posuere imperdiet, leo. Morbi nec metus.
+    </v-alert>
+    <ol>
+      <li v-for="item in items" :key="item.id">{{ item }}</li>
+    </ol>
+  </v-container>
+</template>
+
+<script>
+  export default {
+    data: () => ({ 
+      items: ['title', 'type'] 
+    }), 
+}
+</script>
