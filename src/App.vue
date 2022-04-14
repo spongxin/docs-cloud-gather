efee1c36a131ac9fc2fa1217b7ab792b4cee646e
spongxin <2624490979@qq.com>
2022-04-14 22:50:25 +0800

    添加`qiniu-js`插件

--- src/App.vue
+++ src/App.vue
@@ -6,29 +6,25 @@
           alt="Vuetify Logo"
           class="shrink mr-2"
           contain
-          src="https://cdn.vuetifyjs.com/images/logos/vuetify-logo-dark.png"
+          src="https://file.greenisland.work/static/img/favicon.svg"
           transition="scale-transition"
           width="40"
         />
 
-        <v-img
-          alt="Vuetify Name"
-          class="shrink mt-1 hidden-sm-and-down"
-          contain
-          min-width="100"
-          src="https://cdn.vuetifyjs.com/images/logos/vuetify-name-dark.png"
-          width="100"
-        />
+        <div style="font-weight: bold;font-size: 20px;margin-left: 15px;">
+          <a href="/" style="text-decoration: none;color: white;">Portal Service
+          </a>
+        </div>
       </div>
 
       <v-spacer></v-spacer>
 
       <v-btn
-        href="https://github.com/vuetifyjs/vuetify/releases/latest"
+        href="https://github.com/spongxin/docs-cloud-gather"
         target="_blank"
         text
       >
-        <span class="mr-2">Latest Release</span>
+        <span class="mr-2">GitHub</span>
         <v-icon>mdi-open-in-new</v-icon>
       </v-btn>
     </v-app-bar>
@@ -36,8 +32,20 @@
     <v-main>
       <FileForm/>
     </v-main>
+
+    <v-footer app>
+      <div class="footer">Copyright © <a href="https://github.com/spongxin" style="text-decoration: none;color: black;">SpongXin</a> 2022.</div>
+    </v-footer>
   </v-app>
 </template>
+
+<style type="text/css">
+  .footer {
+    margin: auto;
+    font-size: 14px;
+    color: #757575;
+  }
+</style>
 
 <script>
 import FileForm from './components/FileForm';
